437ec7e36ba349b8b0e86914d4d9c4c92edfbb3c
renddel <61456819+renddel@users.noreply.github.com>
2020-10-28 10:43:29 +0200

    update 0.2

--- app/src/main/java/com/example/rosa/MainActivity.java
+++ app/src/main/java/com/example/rosa/MainActivity.java
@@ -6,10 +6,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,9 +23,14 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-    WebView webHtmlCss;
-    TextView textView;
-    ImageButton imageButton;
+    private WebView webHtmlCss;
+    private TextView textView;
+    private ImageButton imageButton;
+    private TextToSpeech mTTS;
+    private EditText mEditText;
+    private SeekBar mSeekBarSpeed;
+    private SeekBar mSeekBarPitch;
+    private Button mButton;
     private static final int REQUEST_CODE_SPEECH_INPUT = 1000;
 
 
@@ -29,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mButton = findViewById(R.id.buttonSpeak);
         webHtmlCss = (WebView) findViewById(R.id.webView);
         WebSettings ws = webHtmlCss.getSettings();
         ws.setJavaScriptEnabled(true);
@@ -36,42 +47,90 @@ public class MainActivity extends AppCompatActivity {
 
         textView = findViewById(R.id.textTv);
         imageButton = findViewById(R.id.voiceBtn);
-
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 speak();
             }
         });
+
+
+        mTTS = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    int result = mTTS.setLanguage(Locale.ENGLISH);
+                    if (result == TextToSpeech.LANG_MISSING_DATA |
+                            result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                        Log.e("TTS", "Language not supported");
+                    } else {
+                        mButton.setEnabled(true);
+                    }
+
+                } else {
+                    Log.e("TTS", "Initialisation failed");
+                }
+            }
+        });
+        mEditText = findViewById(R.id.speech);
+        mSeekBarPitch = findViewById(R.id.seek_bar_pitch);
+        mSeekBarSpeed = findViewById(R.id.seek_bar_speed);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                talk();
+            }
+        });
+    }
+
+    private void talk() {
+        String text = mEditText.getText().toString();
+        float pitch = (float) mSeekBarPitch.getProgress() / 50;
+        if (pitch < 0.1) pitch = 0.1f;
+
+        float speed = (float) mSeekBarSpeed.getProgress() / 50;
+        if (speed < 0.1) speed = 0.1f;
+        mTTS.setPitch(pitch);
+        mTTS.setSpeechRate(speed);
+        mTTS.speak(text,TextToSpeech.QUEUE_FLUSH, null);
+    }
+
+    @Override
+    protected void onDestroy(){
+        if (mTTS !=null){
+            mTTS.stop();
+            mTTS.shutdown();
+        }
+        super.onDestroy();
     }
 
     private void speak() {
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,"Hi speak smth");
-        try{
-            startActivityForResult(intent,REQUEST_CODE_SPEECH_INPUT);
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hi speak smth");
+        try {
+            startActivityForResult(intent, REQUEST_CODE_SPEECH_INPUT);
 
-        }catch(Exception e){
-            Toast.makeText(this,""+e.getMessage(),Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Toast.makeText(this, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
         }
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
-        super.onActivityResult(requestCode,resultCode,data);
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-        switch(requestCode){
-            case REQUEST_CODE_SPEECH_INPUT:{
-                if(resultCode==RESULT_OK && null!=data){
+        switch (requestCode) {
+            case REQUEST_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
                     ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
                     textView.setText(result.get(0));
                 }
                 break;
             }
         }
-
     }
 
 }
